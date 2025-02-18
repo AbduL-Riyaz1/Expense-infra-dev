6e6c46d5e3078d8fb35cca3961d228840e1d604b
Abdul Riyaz <abdulshaik5801@gmail.com>
2025-02-18 19:57:42 +0530

    terraform Expense-infra-dev

--- 01-vpc/vpc.tf
+++ 01-vpc/vpc.tf
@@ -11,3 +11,15 @@ module "vpc" {
   database_subnet_CIDR = var.database_subnet_CIDR
   is_peering_required = true
 }
+# this can be included in module
+resource "aws_db_subnet_group" "expense" {
+  name       = "${var.project_name}-${var.environment}"
+  subnet_ids = module.vpc.database_subnet_id
+
+  tags =merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}"
+    }
+  )
+}
